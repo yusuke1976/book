de9f629051a7bfb7a897a33756a6de4d7183bb79
yusuke1976 <yusuke_1483@yahoo.co.jp>
2024-06-06 23:42:01 +0900

    bookcard修正

--- indexo.js
+++ indexo.js
@@ -11,6 +11,7 @@ btn.addEventListener('click', async() => {
     bookItemRow.innerHTML = '';
 
     // const bookItem = document.getElementById("bookItem");
+
     for(let i = 0; i < data.items.length; i++){
         // 例外が起きなければtryブロック内のコードが実行される
         try{
@@ -23,35 +24,52 @@ btn.addEventListener('click', async() => {
             const bookContent = data.items[i].volumeInfo.description;
             // 各書籍のGoogle Booksへのリンク
             const bookLink = data.items[i].volumeInfo.infoLink;
-            // 取得したデータを入れるための要素を作成
-            const makeElement = document.createElement("div");
-            // 要素別に識別できるようにidに数字を埋め込む
-            makeElement.setAttribute("id", `bookItem${i}`);
-            // 取得した要素に作成した要素を挿入
-            bookItem.appendChild(makeElement);
-            // 作成した要素を習得
-            const getBookItem = document.getElementById(`bookItem${i}`);
-            // APIで取得したデータの分だけHTML要素を作成し、取得した要素にを埋め込む
-            const setBookElement = `
-                <div class="container">
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <div class="card-body">
-                                <img src="${bookImg}"><br>
-                                <a id="link${i}" class="card-text" target="_blank">${bookTitle}</a>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <p>${bookContent}</p>
-                                </div>
-                            </div>
+            
+            const bookCard = `
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="${bookImg}" class="card-img-top" alt="${bookTitle}">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="${bookLink}" target="_blank">${bookTitle}</a></h5>
+                            <p class="card-text">${bookContent}</p>
                         </div>
                     </div>
                 </div>
             `;
+            
+            
+            // 取得したデータを入れるための要素を作成
+//            const makeElement = document.createElement("div");
+            // 要素別に識別できるようにidに数字を埋め込む
+//            makeElement.setAttribute("id", `bookItem${i}`);
+            // 取得した要素に作成した要素を挿入
+//            bookItem.appendChild(makeElement);
+            // 作成した要素を習得
+//            const getBookItem = document.getElementById(`bookItem${i}`);
+            // APIで取得したデータの分だけHTML要素を作成し、取得した要素にを埋め込む
+//            const setBookElement = `
+            //     <div class="container">
+            //         <div class="col">
+            //             <div class="card shadow-sm">
+            //                 <div class="card-body">
+            //                     <img src="${bookImg}"><br>
+            //                     <a id="link${i}" class="card-text" target="_blank">${bookTitle}</a>
+            //                     <div class="d-flex justify-content-between align-items-center">
+            //                         <p>${bookContent}</p>
+            //                     </div>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // `;
             // APIから取得した、実際のGoogle Booksのサイトに飛ばすためのリンクを埋め込む
-            getBookItem.innerHTML = setBookElement;
-            const link = document.getElementById(`link${i}`);
-            link.href = bookLink;
+            // getBookItem.innerHTML = setBookElement;
+            // const link = document.getElementById(`link${i}`);
+            // link.href = bookLink;
             // 途中で例外が発生した場合はcatchブロック内のコードが実行される
+        
+            bookItemRow.insertAdjacentHTML('beforeend', bookCard);
+        
         }catch(e){
             continue;
         };
